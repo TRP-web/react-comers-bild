dc9b612d83fcb761d4f62aae3f01f390718e49a1
TRP-web <79875751+TRP-web@users.noreply.github.com>
2022-01-26 13:02:26 +0300

    Delete src directory

--- src/components/UI/BuyButton/BuyButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from './BuyButton.module.scss'
-const ButtonBuy = (prop) => {
-  return (
-    <button className={classes.button} {...prop}>{prop.children}</button>
-  )
-}
-
-export default ButtonBuy
--- src/components/UI/Title/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from './Title.module.scss';
-const Title = (prop) => {
-  return (
-    <h3 {...prop} className={classes.title}>{prop.children}</h3>
-  )
-}
-
-export default Title
